38f0d2f39b292fada242fe0f35c7cbde7245898d
Daniel Tran <trandap88@gmail.com>
2019-12-03 06:10:25 -0500

    Moved some files for better organization

--- src/environment/blocks/Blocks.ts
+++
@@ -1,8 +0,0 @@
-import { BlockRegistry } from "./BlockRegistry";
-import { BlockEmpty } from "./base/BlockEmpty";
-import { BlockNull } from "./base/BlockNull";
-
-export const baseRegistry = new BlockRegistry();
-
-baseRegistry.register( BlockEmpty );
-baseRegistry.register( BlockNull );
