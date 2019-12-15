c42d50297a5d5ec3f4b0f912e32499a194b13601
Daniel Tran <trandap88@gmail.com>
2019-12-15 18:20:17 -0500

    Added push and addItem to Inventory

--- src/environment/items/Item.ts
+++ src/environment/items/Item.ts
@@ -1,18 +1,26 @@
 import { Storable } from "../../io/Storable";
 import {baseClass} from "../../utils/Classes";
 
-export type ItemBaseClass = baseClass & {itemId:string};
+export type ItemBaseClass = baseClass & {
+	itemId:string,
+	parseData:(data:any)=>any,
+	stackSize:number
+};
 
 export class Item extends Storable{
 	static module:string = "base";
 	static icon:string = "base:icon:missingItemIcon";
-
+	static stackSize:number = 250;
 	static get itemId(){
 		return `${this.module}:${this.name}`;
 	}
 
 	toStorageObject(){
 		return { className:this.constructor.name };
+	}
+
+	static parseData( data:any ){
+		return data;
 	}
 
 	/**
@@ -24,14 +32,28 @@ export class Item extends Storable{
 export class ItemData extends Storable{
 	baseClass: ItemBaseClass;
 	data: any;
-	constructor(baseClass:ItemBaseClass, data?:any){
+	amount:number = 0;
+	constructor(baseClass:ItemBaseClass, data?:any, amount:number=0){
 		super();
 		this.baseClass = baseClass;
-		this.data = data;
+		this.data = this.baseClass.parseData(data);
+		this.amount = amount;
 	}
 
 	equals(itemData:ItemData){
 		return this.baseClass === itemData.baseClass && this.data == itemData.data;
+	}
+
+	/**
+	 * Returns how many more of this item can be contained within this
+	 * itemData
+	 */
+	freeSlots():number{
+		return Math.max(0, this.baseClass.stackSize - this.amount );
+	}
+
+	equalsInstance(itemData:ItemData){
+		return itemData === this;
 	}
 
 	toStorageObject(){
--- src/items/Inventory.ts
+++ src/items/Inventory.ts
@@ -9,17 +9,60 @@ import { ItemData } from "../environment/items/Item";
  * items.
  */
 class Inventory extends Storable{
-	contents:any[];
+	contents:ItemData[] | null[];
 	size: number;
 	constructor( size:number ){
 		super();
 		this.size = size;
-		this.contents = [];
+		this.contents = new Array(size).fill(null);
 	}
 
+	/**
+	 * Add an item to this inventory. Returns null
+	 * if adding the item resulted in no overflows.
+	 * @param itemData 
+	 */
+	addItem(itemData:ItemData):ItemData|null{
+		// the itemDatas that can merge this itemData
+		let matchedItemDatas = this.contents.filter(( storedItemD )=>{
+			return storedItemD && storedItemD.equals(itemData) && storedItemD.freeSlots();
+		});
 
-	addItem(itemData:ItemData){
-		this.contents
+		if(matchedItemDatas.length){
+			matchedItemDatas.map( (storedItemD)=>{
+				if(!itemData.amount) return;
+				// how much of the item will be taken away from itemData
+				let take = Math.max(itemData.amount, itemData.amount - storedItemD.freeSlots());
+				itemData.amount -= take;
+			});
+		}
+
+		// Returns leftover itemData if item could
+		// not be appended to this inventory
+		if(itemData.amount){
+			return this.push(itemData);
+		}
+
+		// Returns nothing if the item was successfully appended
+		return null;
+	}
+
+	/**
+	 * Push an item to the first
+	 * available inventory slot.
+	 * 
+	 * if no inventory slots are 
+	 * available, this returns
+	 * the itemData.
+	 * @param itemData 
+	 */
+	push(itemData:ItemData){
+		let availableIndex = this.contents.findIndex((x)=>!x);
+		if(availableIndex>-1){
+			this.contents[availableIndex] = itemData;
+		}else{
+			return itemData;
+		}
 	}
 
 }
