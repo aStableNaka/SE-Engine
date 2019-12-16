eec4f5cae75350ef521ea93c47f322499545fb82
Daniel Tran <trandap88@gmail.com>
2019-12-15 22:19:46 -0500

    World.ts cleanup

--- src/environment/world.ts
+++ src/environment/world.ts
@@ -12,20 +12,20 @@ export class World extends Storable{
 	
 	ff: FrostedFlakes;
 	regions!: Grid<Region>
-	regionUpdateQueue:Region[] = [];
+	regionUpdateQueue: Region[] = [];
 
 	worldSize = 4;
 	chunkSize = 16;
 
-	tickLoggingLength:number = 10;
+	tickLoggingLength: number = 10;
 	tickSkip = this.tickLoggingLength*2;
-	tickTime:number = 0;
-	tickCount:number = 0;
-	desiredTPS:number = 20;
-	tickDeltas:number[] = new Array<number>(this.tickLoggingLength).fill(1000/this.desiredTPS);
-	tickScheduler:TickScheduler;
+	tickTime: number = 0;
+	tickCount: number = 0;
+	desiredTPS: number = 20;
+	tickDeltas: number[] = new Array<number>(this.tickLoggingLength).fill(1000/this.desiredTPS);
+	tickScheduler: TickScheduler;
 
-	meshUpdates=6000;
+	meshUpdates:number = 6000;
 
 	constructor( ff:FrostedFlakes ){
 		super();
@@ -40,11 +40,11 @@ export class World extends Storable{
 	/**
 	 * Converts ms to ticks
 	 */
-	msToTicks( ms:number ):number{
+	msToTicks( ms:number ): number {
 		return 1000/this.desiredTPS*ms;
 	}
 
-	queueUpdate( region:Region ){
+	queueUpdate( region:Region ): void {
 		this.regionUpdateQueue.push(region);
 	}
 
@@ -53,16 +53,18 @@ export class World extends Storable{
 	 * 	1 ) Update the camera position
 	 * @abstract
 	 */
-	render(  ){
+	render(  ): void {
 		this.defaultRender();
 	}
 
-	update(){}
+	update(): void {
+
+	}
 
 	/**
 	 * The default rendering cycle
 	 */
-	defaultRender(){
+	defaultRender(): void {
 		let self = this;
 		window.requestAnimationFrame(()=>{
 			self.render();
@@ -70,7 +72,7 @@ export class World extends Storable{
 		self.ff.render();
 	}
 
-	updateTickTimes(){
+	updateTickTimes(): void {
 		let time = new Date().getTime();
 		this.tickCount++;
 		this.tickDeltas[this.tickCount%this.tickDeltas.length] = time-this.tickTime;
@@ -80,7 +82,7 @@ export class World extends Storable{
 	/**
 	 * Gets the average TPS (Ticks Per Second)
 	 */
-	getTPS():number{
+	getTPS(): number {
 		let deltaSum = this.tickDeltas.reduce((pv, cv)=>{
 			return pv+cv;
 		});
@@ -95,13 +97,13 @@ export class World extends Storable{
 	 * Ideally, this will produce a value in MS
 	 * that will keep our tps at ${desiredTPS} (probably 60)
 	 */
-	getNextTickDelay():number{
+	getNextTickDelay(): number {
 		let idealTPS = this.desiredTPS; // ms
 		let currentTPS = this.getTPS();
 		return Math.min(1000/idealTPS, 1000/(idealTPS - (currentTPS - idealTPS)));
 	}
 
-	queueNextTick(){
+	queueNextTick(): void {
 		let nextDelay = this.getNextTickDelay()-3;
 		let tps = this.getTPS();
 		document.title = tps.toFixed(2) + " - - - " + this.tickCount + " - - - " + 1000/tps+" / "+1000/this.desiredTPS;
@@ -113,7 +115,7 @@ export class World extends Storable{
 		}, nextDelay);
 	}
 
-	getRegionAtVec2(vec2:THREE.Vector2){
+	getRegionAtVec2(vec2: THREE.Vector2): Region | null {
 		let pos = vec2.clone().divideScalar(this.chunkSize).floor();
 		return this.regions.get(pos.x, pos.y);
 	}
@@ -123,7 +125,7 @@ export class World extends Storable{
 		Map manipulation
 	*/
 
-	setBlock( blockData:BlockData, x:number, y:number, z:number ){
+	setBlock( blockData:BlockData, x:number, y:number, z:number ): void {
 		let region = this.getRegionAtVec2(new Vector2(x,y));
 		if(!region) throw new Error(`[SimonsWorld] Attempted to set block out of bounds ${x},${y},${z}`);
 		this.regionUpdateQueue.push(region);
@@ -139,7 +141,7 @@ export class World extends Storable{
 	 * 	1 ) Update mesh transforms
 	 * 	2 ) Update region states
 	 */
-	tick(){
+	tick(): void {
 		if(this.tickSkip){
 			this.tickSkip--;
 		}else{
@@ -149,26 +151,7 @@ export class World extends Storable{
 				if(!reg) return;
 				reg.update(  )
 			}
-			/*if(this.meshUpdates && false){
-				this.meshUpdates--;
-				this.ff.children.map((child)=>{
-					child.traverse((o3d)=>{
-						if(o3d.type=="Mesh"){
-							let im = (<THREE.Mesh>o3d);
-							let bg = <THREE.BufferGeometry>im.geometry
-							if(bg.attributes){
-								if( bg.attributes.position && bg.attributes.normals && bg.attributes.uv ){
-									(<THREE.BufferAttribute>bg.attributes.position).needsUpdate = true;
-									(<THREE.BufferAttribute>bg.attributes.normals).needsUpdate = true;
-									(<THREE.BufferAttribute>bg.attributes.uv).needsUpdate = true;
-								}
-							}
-						}
-					})
-				})
-			}*/
 		}
-		
 		this.queueNextTick();
 	}
 }
